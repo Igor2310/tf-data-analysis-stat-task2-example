7713462c9d6c3cb2d88f604623dd838894b19caa
Igor2310 <80477307+Igor2310@users.noreply.github.com>
2023-04-08 01:46:49 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,27 +2,13 @@ import pandas as pd
 import numpy as np
 
 from scipy.stats import norm
+from scipy.stats import chi2
+
 
 
 chat_id = 1152225195 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    t = x.max()
-    if len(x) == 1000 and p == 0.99:
-        l = 2.47
-        return (t, t+l)
-    if len(x) == 1000 and p == 0.9:
-        l = 0.790
-        return (t, t+l)
-    if len(x) == 100 and p == 0.7:
-        l = 0.300
-        return (t, t+l)
-    if len(x) == 100 and p == 0.9:
-        l = 0.250
-        return (t, t+l)
-    if len(x) == 10 and p == 0.95:
-        l = 1.51
-        return (t, t+l)
-    if len(x) == 10 and p == 0.9:
-        l = 0.899
-        return (t, t+l)
+  n = len(x)
+  return np.sqrt(sum(x**2)/ (14*chi2.ppf((1+p)/2, df = 2*n))), \
+  np.sqrt(sum(x**2)/ (14*chi2.ppf((1 - p) / 2, df = 2*n ))) 
